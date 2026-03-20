ee1dc3de94053971cd7ca6ccf19c794c911e1124
nolongeraregistereduser <mohamedzouhairi023@gmail.com>
2026-03-20 16:58:48 +0000

    Micro services @ALL

---
+++ invoice-service/src/test/java/com/facturationservice/demo/FacturationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facturationservice.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
